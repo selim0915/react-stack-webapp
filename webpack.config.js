727d1fdc589f00ff7a0e2ed50f690109f2944050
DESKTOP-SUM2U0C\G3A211104 <selim0915@gmail.com>
2026-09-17 08:14:11 +0900

    chore: 웹팩 설정 및 src/ 폴더 구조 변경

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,67 @@
 require('dotenv').config();
 const path = require('path');
 const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const isProduction = process.env.NODE_ENV === 'production';
+const srcPath = path.resolve(__dirname, 'src');
 
 module.exports = {
-  mode: 'production',
-  entry: './front/src/index.tsx',
+  mode: isProduction ? 'production' : 'development',
+  entry: './src/index.tsx',
   output: {
     filename: '[name].[contenthash].js',
     path: path.resolve(__dirname, './dist'),
     publicPath: '/',
     clean: true,
   },
-  devtool: false,
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+    alias: {
+      '@': srcPath,
+    },
+  },
+  devtool: isProduction ? false : 'source-map',
+  devServer: {
+    port: 4002,
+    host: '0.0.0.0',
+    historyApiFallback: true,
+    server: {
+      type: 'https',
+    },
+    proxy: {
+      '/api': {
+        target: 'https://localhost:4000',
+        pathRewrite: { '^/api': '' },
+        secure: false,
+      },
+      '/api2': {
+        target: 'https://localhost:4001',
+        pathRewrite: { '^/api2': '' },
+        secure: false,
+      },
+    },
+  },
   module: {
     rules: [
       {
         test: /\.(ts|js)x?$/i,
         exclude: /node_modules/,
-        use: 'babel-loader',
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript']
+          }
+        }
       },
       {
         test: /\.(sa|sc|c)ss$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+        use: [
+          isProduction ? MiniCssExtractPlugin.loader : 'style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
       },
       {
         test: /\.(jpe?g|gif|png|svg|ico)$/,
@@ -33,29 +71,25 @@ module.exports = {
             maxSize: 8 * 1024,
           },
         },
+        generator: {
+          filename: 'assets/images/[name][ext]'
+        }
       },
     ],
   },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-    alias: {
-      '@': path.resolve(__dirname, 'front/src'),
-    },
-  },
   plugins: [
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('production'),
+      'process.env.NODE_ENV': JSON.stringify(isProduction ? 'production' : 'development'),
       'process.env.SENTRY_DSN': JSON.stringify(process.env.SENTRY_DSN),
       'process.env.GA_MEASUREMENT_ID': JSON.stringify(process.env.GA_MEASUREMENT_ID),
     }),
     new HtmlWebpackPlugin({
-      title: 'React Stack WebApp',
-      template: './front/public/index.html',
-      favicon: './front/public/favicon.ico',
-      minify: true,
-    }),
-    new MiniCssExtractPlugin({
-      filename: '[name].[contenthash].css',
+      template: './public/index.html',
+      favicon: './public/favicon.ico',
+      minify: {
+        collapseWhitespace: true,
+        removeComments: true
+      }
     }),
   ],
   optimization: {
